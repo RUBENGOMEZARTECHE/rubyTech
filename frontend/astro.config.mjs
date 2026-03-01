07f3bcd09da47895f2c7ec4955c32e089a22faa9
RUBENGOMEZARTECHE <rubgoar@gmail.com>
2026-03-01 16:08:59 +0000

    feat: add sitemap.xml and robots.txt

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'http://46.62.132.241',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
+
